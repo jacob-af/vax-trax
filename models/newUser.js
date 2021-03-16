a7c177c894fd98705284f8b7a8353ac350f40213
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-03-15 20:36:28 -0400

    possible set up for newUser

---
+++ models/newUser.js
@@ -0,0 +1,33 @@
+const { INTEGER } = require("sequelize/types");
+
+// eslint-disable-next-line camelcase
+module.exports = function(vax_trax_db, DataTypes) {
+  // eslint-disable-next-line camelcase
+  const NewUser = vax_trax_db.define("NewUser", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    vaccineType: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    vaccineInterest: {
+      type: DataTypes.Boolean,
+      allowNull: true
+    },
+    schuduled: {
+      type: DataTypes.Boolean,
+      allowNull: true
+    },
+    shotNumber: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    blog: {
+      type: DataTypes.STRING(1500),
+      allowNull: true
+    }
+  });
+  return NewUser;
+};
