6e913a68752950ec9a44ad0506f20a3863759380
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-03-20 10:30:00 -0400

    fixed up userStory.js and the user.js hasMany

--- models/user.js
+++ models/user.js
@@ -24,6 +24,9 @@ module.exports = function(sequelize, DataTypes) {
     User.hasMany(models.userStory, {
       onDelete: "cascade"
     });
+    User.hasMany(models.userData, {
+      onDelete: "cascade"
+    });
   };
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
