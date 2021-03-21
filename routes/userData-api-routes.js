c735f06bda9afc4c1395f154046fa7623d8c3eef
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-03-21 17:33:14 -0400

    another similar commit

--- routes/userData-api-routes.js
+++ routes/userData-api-routes.js
@@ -18,9 +18,11 @@ module.exports = app => {
   app.put("/api/user", (req, res) => {
     db.UserData.update(
       {
+        name: req.body.name,
+        vaccineInterest: req.body.vaccineInterest,
+        vaccineType: req.body.vaccineType,
         scheduled: req.body.scheduled,
-        shotNumber: req.body.shotNumber,
-        vaccineType: req.body.vaccineType
+        shotNumber: req.body.shotNumber
       },
       {
         where: {
