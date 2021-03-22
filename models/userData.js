66e3f96c80b051e139ed20680b5cfdfd32995232
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-03-22 19:04:01 -0400

    fixed userData table and the User table.

--- models/userData.js
+++ models/userData.js
@@ -4,22 +4,18 @@
 module.exports = function(vax_trax_db, DataTypes) {
   // eslint-disable-next-line camelcase
   const userData = vax_trax_db.define("userData", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    vaccineInterest: {
-      type: DataTypes.BOOLEAN,
-      allowNull: true
-    },
     vaccineType: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: true
     },
-    schuduled: {
+    scheduled: {
       type: DataTypes.BOOLEAN,
       allowNull: true
     },
+    datesScheduled:{
+      type: DataTypes.STRING,
+      allowNull: true
+    }
     shotNumber: {
       type: DataTypes.INTEGER,
       allowNull: true
